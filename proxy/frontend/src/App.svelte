1c6e835a2fe67f3383dc3defd640449b653ab09b
M1chaCH <micha_ch@outlook.com>
2023-06-12 22:02:48 +0200

    style frontend

--- proxy/frontend/src/App.svelte
+++ proxy/frontend/src/App.svelte
@@ -6,7 +6,10 @@
 
 	isLoggedIn()
 	.then(loggedInUser => user.set(loggedInUser))
-	.catch(e => console.log(e));
+	.catch(_ => console.log("failed to check login status"));
+
+	let viewPrivate = false;
+	user.subscribe(u => viewPrivate = u?.viewPrivate || false);
 
 	async function getPages() {
 		const response = await fetch(`${apiUrl}/pages`);
@@ -27,16 +30,39 @@
 </script>
 
 <svelte:head>
-	<title>Overview</title>
+	<title>Micha Schweizer @ Home</title>
 </svelte:head>
 <main>
-	<h1>Overview</h1>
+	<h1>Micha Schweizer @ Home</h1>
+	<p style="max-width: 500px; font-size: 20px; font-weight: 300; padding-bottom: 5vh;">
+		This page shows all applications that are deployed on my server. Feel free to explore and
+		get to know my projects more closely.
+		<br />
+		<span class="small-text">Some might me locked, you need to have special access for these.</span>
+	</p>
 	{#await getPages()}
 		<p>Loading pages ...</p>
 	{:then pages}
-		{#each pages as page}
-			<p>{page.title}</p>
-		{/each}
+		<div class="pages">
+			{#each pages as page}
+				<a class="page" class:disabled-page={!viewPrivate && page.private_access} href={page.url}>
+					<h3>
+						{page.title}
+						<span class="small-text">{page.url}</span>
+					</h3>
+					<p class="text" style="">{page.description}</p>
+					<span style="display: none">{page.private_access}</span>
+
+					{#if page.private_access}
+						{#if viewPrivate}
+							<span class="material-symbols-rounded lock">lock_open</span>
+						{:else }
+							<span class="material-symbols-rounded lock">encrypted</span>
+						{/if}
+					{/if}
+				</a>
+			{/each}
+		</div>
 	{:catch error}
 		<p>Could not load pages: {error.message}</p>
 	{/await}
@@ -45,12 +71,56 @@
 		<p>{$user.mail}</p>
 	{:else }
 		<div>
-			<h3>Login to see more pages</h3>
-			<Login bind:apiUrl/>
+<!--			<h3>Login to see more pages</h3>-->
+<!--			<Login bind:apiUrl/>-->
 		</div>
 	{/if}
 </main>
 
 <style>
+	.pages {
+		display: flex;
+		flex-flow: row wrap;
+		gap: 20px;
+	}
 
+	.pages .page {
+		all: unset;
+		position: relative;
+
+		padding: 20px;
+		width: 350px;
+		background-color: #2A9D8F;
+		box-shadow: inset 0 0 0 4px #264653;
+
+		cursor: pointer;
+		transition: all 250ms ease-out;
+	}
+
+	.disabled-page {
+		pointer-events: none !important;
+	    background-color: color-mix(in srgb, #2A9D8F 25%, transparent) !important;
+	    box-shadow: inset 0 0 0 4px color-mix(in srgb, #264653 60%, white) !important;
+	}
+
+	.pages .page:hover {
+		transition: all 250ms ease-out;
+		background-color: #264653;
+
+		box-shadow: inset -10px -10px 0 10px #2A9D8F;
+	}
+
+	.pages .page:hover h3,
+	.pages .page:hover span,
+	.pages .page:hover p {
+		color: white;
+	}
+
+	.lock {
+		position: absolute;
+		top: 10px;
+		right: 10px;
+
+		font-size: 40px;
+	}
 </style>
